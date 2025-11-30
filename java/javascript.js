e6e8ebfc400583a42dd02c107ad81e7562bf0004
AbhinashDhakal <abinashdhkal1234@gmail.com>
2025-11-30 13:09:54 +0545

    java starting

---
+++ java/javascript.js
@@ -0,0 +1,8 @@
+address="putalisadak"
+reciever="Abhinash Dhakal"
+Name="Abhinash"
+console.log("Name:", Name)
+console.log("putalisadak" );
+contactnumber="9748434128"
+console.log(contactnumber)
+// var let var and const var
