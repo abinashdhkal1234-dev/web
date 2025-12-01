285b6e433d0729889052bcad6b3638738eb69bd7
AbhinashDhakal <abinashdhkal1234@gmail.com>
2025-12-01 18:29:28 +0545

    js starting

--- java/javascript.js
+++ java/javascript.js
@@ -1,12 +1,23 @@
-let address="putalisadak"
-reciever="Abhinash Dhakal"
-Name="Abhinash"
-console.log("Name:", Name)
-console.log("putalisadak" );
-contactnumber="9748434128"
-console.log(contactnumber)
-// var, let var and const var
-const PI = 3.1415
-console.log(PI)
-// dont mix different datatypes
- 
+let subjects = ["mer", "python", "qa"];
+
+console.log("prev", subjects[0]);
+console.log(subjects[1]);
+console.log(subjects[2]);
+
+subjects[0] = "mern-stack";
+
+console.log("after", subjects[0]);
+console.log("after", subjects);
+
+console.log(subjects[3])
+subjects[3] = "machine learning"
+
+
+/* 
+    TODO: when printing subjects
+    i need more detail information of each subjects.
+        - price
+        - start data
+        - end date
+        - time
+*/
