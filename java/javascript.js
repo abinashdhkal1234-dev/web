fc0c8f92c16b48543373cb65e5f37402494a99e9
AbhinashDhakal <abinashdhkal1234@gmail.com>
2025-12-01 09:06:36 +0545

    js class 3

--- java/javascript.js
+++ java/javascript.js
@@ -1,8 +1,12 @@
-address="putalisadak"
+let address="putalisadak"
 reciever="Abhinash Dhakal"
 Name="Abhinash"
 console.log("Name:", Name)
 console.log("putalisadak" );
 contactnumber="9748434128"
 console.log(contactnumber)
-// var let var and const var
+// var, let var and const var
+const PI = 3.1415
+console.log(PI)
+// dont mix different datatypes
+ 
